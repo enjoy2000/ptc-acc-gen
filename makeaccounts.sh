ae6fc6ec4424965912379f7bb60794b68e86783f
pyr0ball <pyroballpcs@gmail.com>
2016-08-09 15:30:48 -0700

    automated backup of old accounts.csv
    renames old accounts.csv file to avoid PogoPlayer attempting to run accounts that are already active

--- makeaccounts.sh
+++ makeaccounts.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-
+name=accounts
+if [[ -e $name.csv ]] ; then
+    i=0
+    while [[ -e $name-$i.csv ]] ; do
+        let i++
+    done
+    name=$name-$i
+fi
 DEBUG=nightmare xvfb-run --server-args="-screen 0 1024x768x24" node index.js
 cd PogoPlayer
 xvfb-run --server-args="-screen 0 1024x768x24" node pogo.js
